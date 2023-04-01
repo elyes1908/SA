ea97306ed4b523cf7e68340b14f5e18578dd1807
elyes1908 <elyes.tarmiz@outlook.fr>
2023-04-01 21:52:38 +0100

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installation
+
+### Install project dependencies
+Run `pip install -r requirements.txt`
+### Create database
+Run  `python .\create_database.py`
+## Run the app
+Run `python .\app.py`
---
+++ create_database.py
@@ -0,0 +1,10 @@
+from app import db
+
+db.create_all()
+if __name__ == '__main__':
+    try:
+        db.create_all()
+        print('Database created.')
+    except Exception as e:
+        print('Database creation failed.')
+        print(e)
